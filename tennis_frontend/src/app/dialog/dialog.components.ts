7be6911980b9e6baae6f05dd62063c33a95c3821
bwei2121 <brandon.wei1221@gmail.com>
2024-03-28 09:02:47 -0400

    Add comments to match dialog component

--- tennis_frontend/src/app/dialog/dialog.components.ts
+++ tennis_frontend/src/app/dialog/dialog.components.ts
@@ -44,6 +44,9 @@ export class MatchOverviewDialog implements OnInit {
     private formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
+  /**
+   * Sets up match information statistics and match prediction panels
+   */
   async ngOnInit(): Promise<void> {
     const player1 = this.formBuilder.group({ player: [this.data.player1.playerName], firstSet: [''], secondSet: [''], thirdSet: [''] });
     const player2 = this.formBuilder.group({ player: [this.data.player2.playerName], firstSet: [''], secondSet: [''], thirdSet: [''] });
@@ -52,9 +55,9 @@ export class MatchOverviewDialog implements OnInit {
     this.tableDataSourcePrediction = new MatTableDataSource((this.tennisScores.get('playerScore') as FormArray).controls);
     const h2hRequest=await axios.get("http://localhost:8000/player", {
       params: {
-        player: this.parsePlayerName(this.data.player1.playerName, ''),
-        opponent: this.parsePlayerName(this.data.player2.playerName, ' '),
-        opponentParsed: this.parsePlayerName(this.data.player2.playerName, '')
+        player: this.modifyPlayerName(this.data.player1.playerName, ''),
+        opponent: this.modifyPlayerName(this.data.player2.playerName, ' '),
+        opponentParsed: this.modifyPlayerName(this.data.player2.playerName, '')
       }
     });
     const playersData: PlayersData=h2hRequest.data;
@@ -64,22 +67,37 @@ export class MatchOverviewDialog implements OnInit {
     const tourType=playersData.tourType;
     this.h2hLoaded=true;
     const tableDataSourceInformation: MatchInfoDialog[] = [
-      {player: this.data.player1.playerName, playerParsed: this.parsePlayerName(this.data.player1.playerName, ''), opponent: this.parsePlayerName(this.data.player2.playerName, ''), h2h: h2h.wins, h2hOpp: h2h.losses, rank: player1Rank, tourType: tourType},
-      {player: this.data.player2.playerName, playerParsed: this.parsePlayerName(this.data.player2.playerName, ''), opponent: this.parsePlayerName(this.data.player1.playerName, ''), h2h: h2h.losses, h2hOpp: h2h.wins, rank: player2Rank, tourType: tourType}
+      {
+        player: this.data.player1.playerName, playerParsed: this.modifyPlayerName(this.data.player1.playerName, ''), 
+        opponent: this.modifyPlayerName(this.data.player2.playerName, ''), h2h: h2h.wins, h2hOpp: h2h.losses, 
+        rank: player1Rank, tourType: tourType
+      },
+      {
+        player: this.data.player2.playerName, playerParsed: this.modifyPlayerName(this.data.player2.playerName, ''), 
+        opponent: this.modifyPlayerName(this.data.player1.playerName, ''), h2h: h2h.losses, h2hOpp: h2h.wins, 
+        rank: player2Rank, tourType: tourType
+      }
     ]
     this.tableDataSourceInformation = new MatTableDataSource(tableDataSourceInformation);
   }
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
-  setPlayerWinner(player: string, playerNumber: number): void{
+  /**
+   * Sets player winner based on user prediction
+   * @param player: name of predicted winning player 
+   * @param playerNumber: 1 or 2, top player or bottom player as displayed in popup and in bracket respectively 
+   * Returns void
+   */
+  setPlayerWinner(player: string, playerNumber: number): void {
     this.data.playerWinner=player;
     this.winPlayerNumber=playerNumber;
     this.losePlayerNumber=this.winPlayerNumber ^ 1;
   }
 
+  /**
+   * Modify winning and loser scores to align with scores stored in tennis bracket and 
+   * send data to bracket component 
+   * @returns void
+   */
   sendData(): void {
     const winningScores = this.playerScore.controls[this.winPlayerNumber].value;
     const losingScores = this.playerScore.controls[this.losePlayerNumber].value;
@@ -88,7 +106,13 @@ export class MatchOverviewDialog implements OnInit {
     this.dialogRef.close(this.data);
   }
 
-  parsePlayerName(playerName: string, joinString: string): string {
+  /**
+   * Modify player names, used for displaying name in dialog and url searches on backend to get player/match data
+   * @param playerName: name of player 
+   * @param joinString: string used to modify player name 
+   * @returns string: modified player name
+   */
+  modifyPlayerName(playerName: string, joinString: string): string {
     let nameArray=playerName.split(' ');
     if(nameArray.length>=1 && nameArray[0].includes('(') && nameArray[0].includes(')')){
       nameArray.splice(0, 1);
